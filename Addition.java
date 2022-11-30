1b16a3c052e9e9a3ee44a97e4a141fab7e3d303d
subhankarray <subhankarraypp32@gmail.com>
2022-12-01 01:21:00 +0530

    Added another function for Input

--- Addition.java
+++ Addition.java
@@ -1,6 +1,18 @@
 import java.util.*;
 class Addition
 {
+        int input()
+         {
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter 1st-Number:");
+            int a=sc.nextInt();
+            System.out.println();
+	    System.out.print("Enter 2nd-Number:");
+	    int b=sc.nextInt();
+	    System.out.println();
+            return sum(a,b);  
+         }
+
 	int sum(int x,int y)
 	{
 		return (x+y);
@@ -8,14 +20,7 @@ class Addition
 
 	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
-		Addition ob=new Addition();
-		System.out.print("Enter 1st-Number:");
-                int a=sc.nextInt();
-		System.out.println();
-		System.out.print("Enter 2nd-Number:");
-		int b=sc.nextInt();
-		System.out.println();
-	        System.out.println("Sum:"+ob.sum(a,b));
+                Addition ob=new Addition();
+	        System.out.println("Sum:"+ob.input());
 	}
 }
